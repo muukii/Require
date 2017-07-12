f782154e1d0aea60b29944d89c62d62485e7dade
muukii <m@muukii.me>
2017-07-13 00:44:24 +0900

    init

---
+++ Sources/Require/Require.h
@@ -0,0 +1,19 @@
+//
+//  Require.h
+//  Require
+//
+//  Created by muukii on 7/13/17.
+//  Copyright © 2017 muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Require.
+FOUNDATION_EXPORT double RequireVersionNumber;
+
+//! Project version string for Require.
+FOUNDATION_EXPORT const unsigned char RequireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Require/PublicHeader.h>
+
+
